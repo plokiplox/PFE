d111deadb7dfa8ae745771085bbe0821c02981c5
philip <philip@philip-VirtualBox>
2019-10-15 01:50:50 -0400

    Ajouté le main.py et tester les classes avec un actionneur.

---
+++ Sources/Classes/Actionneurs.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct. 15, 2019
+
+@author: philip
+'''
+
+class Actionneurs:
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        self.IO = params
+    pass
+
+    def GetIO(self):
+        return self.IO;
---
+++ Sources/Main.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct. 15, 2019
+
+@author: philip
+'''
+from Classes.Actionneurs import Actionneurs
+
+if __name__ == '__main__':
+    
+    A = Actionneurs(23)
+    
+    print A.GetIO()
+    
+    pass
