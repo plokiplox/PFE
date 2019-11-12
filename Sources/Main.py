50c5d7680e2845e4e8f73876c309cfc998a77d5b
Philip Tittley <tittleyphilip@gmail.com>
2019-11-12 15:32:42 -0500

    Serveur ajouté, thread pondoire fait et init fait

--- Sources/Main.py
+++ Sources/Main.py
@@ -5,27 +5,12 @@ Created on Oct. 15, 2019
 
 @author: philip
 '''
-#from Classes.Poulailler import Poulailler
-from Classes.Capteurs import Obscurite
-from Classes.Actionneurs import LumiereLED
-import time
+
+import Classes.Serveur
 
 if __name__ == '__main__':
     
-    #A = Poulailler()
-    #A.InitialisationThreads()
+    A = Classes.Serveur()
+    A.__start__()
     
-    light = Obscurite(25)
-    led = LumiereLED(24)
-    
-    while True:
-        if light.LectureCapteur():
-            led.Allumer()
-            print("Light Detected")
-        else:
-            led.Fermer()
-            print("No Light")
-            
-        time.sleep(1)
-        continue
     pass
