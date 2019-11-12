b7f013c955d96ac84c239f81636c295bbb801f47
Philip Tittley <tittleyphilip@gmail.com>
2019-11-12 15:52:43 -0500

    Thread distribution d'eau ajouté et init, enlevé le test dans thermostat

--- Sources/Classes/Distribution_Eau.py
+++ Sources/Classes/Distribution_Eau.py
@@ -10,10 +10,10 @@ from Classes.Actionneurs import ElementChauffant
 from Classes.Actionneurs import ElectroVanne
 from Classes.Capteurs import Temperature
 from Classes.Capteurs import Niveau
-
+import threading.Thread
 import time
 
-class Distribution_Eau:
+class Distribution_Eau(threading.Thread):
     '''
     Classe pour contrôler le système de distribution d'eau dans le poulailler
     '''
@@ -23,12 +23,15 @@ class Distribution_Eau:
     Tempearture_Reservoire_Min = 3
     Temperature_Reservoire_Max = 8
     # Secondes
-    Delai_temp = 5
+    Delai_temps_heater = 5
+    Delai_temps_remplissage = 1
 
     def __init__(self, IO_HeaterReservoire,IO_CTemp_Reservoire,IO_HeaterCanalisation, IO_CTemp_Canalisation, IO_ElectroVanne, IO_CNiveau_Haut, IO_CNiveau_Bas):
         '''
         Constructeur
         '''
+        threading.Thread.__init__(self, target=self.Actions())
+        
         self.Vanne = ElectroVanne(IO_ElectroVanne)
         
         self.Heater_Reservoire = ElementChauffant(IO_HeaterReservoire)
@@ -44,14 +47,34 @@ class Distribution_Eau:
         self.Heater_Reservoire.Marche()
         
         while self.CTemp_Reservoire.GetTemperature() < self.Temperature_Max:
-            time.sleep(self.Delai_temp)
+            time.sleep(self.Delai_temps_heater)
         
         self.Heater_Reservoire.Arret()
         
     def RechaufferCanalisation(self):
-        pass
+        self.Heater_Canalisation.Marche()
+        
+        while self.CTemp_Canalisation.GetTemperature() < self.Temperature_Max:
+            time.sleep(self.Delai_temps_heater)
+        
+        self.Heater_Canalisation.Arret()
     
     def RemplirReservoire(self):
+        self.Vanne.Ouverture()
         
+        while not self.CNiveau_Haut.LectureCapteur():
+            time.sleep(self.Delai_temps_remplissage)
+            
+        self.Vanne.Fermeture()
+    
+    def Actions(self):
         
-        pass
+        while True:
+            if not self.CNiveau_Bas:
+                self.RechaufferCanalisation()
+                self.RemplirReservoire()
+            else:
+                if self.CTemp_Reservoire.GetTemperature() < self.Tempearture_Reservoire_Min:
+                    self.RechaufferReservoire()
+        continue
+        
--- Sources/Classes/Thermostat.py
+++ Sources/Classes/Thermostat.py
@@ -39,8 +39,7 @@ class Thermostat(threading.Thread):
         self.Heater.Arret()
         
     def Actions(self):
-        #self.BoucleAction()
-        self.threadtest()
+        self.BoucleAction()
         pass
     
     def BoucleAction(self):
@@ -53,14 +52,3 @@ class Thermostat(threading.Thread):
                     time.sleep(self.Delai_temp)
                     continue
             continue
-    
-    def threadtest(self):
-        print("Thread Started")
-        t=0
-        while t != 10:
-            print(t," Seconds")
-            time.sleep(1)
-            t +=1
-            continue
-        print("Thread Finished")
-        pass
